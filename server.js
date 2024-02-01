fafb2b11cf1d84543786b56cbbf68dee5fd5ac26
Spec-Tr <142240560+Spec-Tr@users.noreply.github.com>
2024-02-01 02:22:03 -0500

    adding dependencies, etc.

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require(`express`);
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware 
+app.use(express.static(`public`));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+const allRoutes = require(`./controllers`);
+app.use(allRoutes);
+
+app.listen(PORT, () => {
+	console.log(`App listening at http://localhost:${PORT}`);
+});
