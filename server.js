f96c958b3b19b41ed03886a7bce7b906f6ea7e21
Spec-Tr <142240560+Spec-Tr@users.noreply.github.com>
2024-02-01 12:44:39 -0500

    removing unused CORS

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require(`express`);
-const cors = require(`cors`);
-const fs = require(`fs`);
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware 
 app.use(express.static(`public`));
-app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
