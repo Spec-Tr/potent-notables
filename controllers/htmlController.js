5f4fbeee60acef8d5f9776382fdfccfe6f5a6e89
Spec-Tr <142240560+Spec-Tr@users.noreply.github.com>
2024-01-31 23:32:07 -0500

    added htmlControllers file

---
+++ controllers/htmlController.js
@@ -0,0 +1,20 @@
+const express = require(`express`);
+const router = express.Router();
+const path = require(`path`);
+
+//Request for homepage
+router.get(`/`, (req, res) => {
+    res.sendFile(path.join(__dirname, `../public/index.html`))
+});
+
+//Request for notes page
+router.get(`/notes`, (req, res) => {
+    res.sendFile(path.join(__dirname, `../public.notes.html`))
+});
+
+//Request for *
+router.get(`*`,(req, res) => {
+    res.sendFile(path.join(__dirname, `../public/index.html`))
+});
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
+const htmlRoutes = require("./htmlController")
+router.use(htmlRoutes)
+
 module.exports = router;
